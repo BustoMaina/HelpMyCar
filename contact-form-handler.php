f405285f8ee28f0d8b08611f60239bf25aa8fd71
Emil Petkov <epetkov.dev@gmail.com>
2019-08-10 12:43:38 +0300

    contact form update with php

---
+++ contact-form-handler.php
@@ -0,0 +1,24 @@
+<?php
+$name = $_POST['name'];
+$visitor_email = $_POST['email'];
+$phone = $_POST['telephone'];
+$message = $_POST['message'];
+
+$email_form = 'helpmycar@tst-dev.com';
+
+$email_subject = "New Form Submission";
+
+$email_body = 
+    "Имена: $name.\n".
+        "Email: $visitor_email.\n".
+            "Телефон: $phone.\n".
+                "Съобщение: $message.\n";
+
+
+$to = "epetkov.dev@gmail.com";
+$headers = "From: $email_form \r\n";
+$headers .= "Reply-To: $visitor_email \r\n";
+mail($to,$email_subject,$email_body,$headers);
+header("Location: index.html");
+
+?>
